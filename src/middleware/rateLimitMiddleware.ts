28b65614d1a60f4516f997f926cfd29add9e5dbf
Gabriel-Salvarani <gabrielsalvarani@gmail.com>
2025-11-20 16:00:12 -0300

    fix

--- src/middleware/rateLimitMiddleware.ts
+++ src/middleware/rateLimitMiddleware.ts
@@ -1,14 +1,15 @@
-import rateLimit from "express-rate-limit"
+import rateLimit from "express-rate-limit";
+import { Request, Response, NextFunction } from "express";
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,// 15 minutos,
+  windowMs: 15 * 60 * 1000,
   max: 5,
-  handler: (req, res, next, options) => {
+  handler: (req: Request, res: Response, next: NextFunction, options: any) => {
     res.status(429).json({
       success: false,
-      error: `Limite alcanzado ${options.max} solicitudes cada ${options.windowMs / 1000 / 60} minutos.`
-    })
+      error: `Límite alcanzado ${options.max} solicitudes cada ${options.windowMs / 1000 / 60} minutos.`
+    });
   }
-})
+});
 
-export default limiter
+export default limiter;
