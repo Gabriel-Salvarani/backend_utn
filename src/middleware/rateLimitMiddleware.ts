339879464203d08cc2fc01f82a36db65bbee8dd4
Gabriel-Salvarani <gabrielsalvarani@gmail.com>
2025-11-20 15:19:23 -0300

    Arreglando errores

--- src/middleware/rateLimitMiddleware.ts
+++ src/middleware/rateLimitMiddleware.ts
@@ -1,20 +1,14 @@
-// ratelimitMiddleware.ts
+import rateLimit from "express-rate-limit"
 
-import { Request, Response, NextFunction } from "express"; 
-// Importa Options para usar el tipo oficial
-import rateLimit, { RateLimitRequestHandler, Options } from "express-rate-limit"; 
-
-const limiter: RateLimitRequestHandler = rateLimit({
-  windowMs: 60 * 1000, 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,// 15 minutos,
   max: 5,
-// Usa el tipo 'Options' importado.
-  handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
+  handler: (req, res, next, options) => {
     res.status(429).json({
       success: false,
- // options es ahora de tipo Options oficial
       error: `Limite alcanzado ${options.max} solicitudes cada ${options.windowMs / 1000 / 60} minutos.`
     })
   }
-});
+})
 
-export default limiter;
+export default limiter
